d8b15fe457407dabaa7d045c0c4c6eef7ad73bb3
thbe <project@bendler-net.de>
2015-10-22 16:01:27 +0200

    Prepare Travis CI integration, part 7, basic tests

--- spec/classes/ssmtp_spec.rb
+++ spec/classes/ssmtp_spec.rb
@@ -5,10 +5,14 @@ describe 'ssmtp', :type => :class do
   describe "ssmtp class with no parameters, basic test" do
     let(:params) { { } }
 
-      it {
-        should contain_package('ssmtp')
-        should contain_file('/etc/ssmtp/ssmtp.conf')
-        should contain_file('/etc/ssmtp/revaliases')
-      }
+      it { should contain_class('ssmtp::params') }
+      it { should contain_class('ssmtp::package') }
+      it { should contain_class('ssmtp::config') }
+      it { should contain_class('ssmtp::service') }
+      it { should contain_package('ssmtp') }
+      it { should contain_file('/etc/ssmtp/ssmtp.conf') }
+      it { should contain_file('/etc/ssmtp/revaliases') }
+      it { shoudl contain_exec('alternatives --set mta /usr/sbin/sendmail.ssmtp') }
+
   end
 end
