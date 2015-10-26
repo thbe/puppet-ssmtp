efd39d6d379a6c4f6e07ae33ab3b9279311a5c57
thbe <project@bendler-net.de>
2015-10-26 16:09:29 +0100

    Add content test

--- spec/classes/ssmtp_spec.rb
+++ spec/classes/ssmtp_spec.rb
@@ -8,6 +8,14 @@ describe "ssmtp" do
         facts
       end
 
+      let(:params) {
+        {
+          :default_mta => 'ssmtp',
+          :root_email => 'test@example.local',
+          :mail_hub => 'mh.example.local'
+        }
+      }
+
       it { is_expected.to compile.with_all_deps }
 
       case facts[:osfamily]
@@ -18,6 +26,14 @@ describe "ssmtp" do
         it { is_expected.to contain_file('/etc/ssmtp/ssmtp.conf') }
         it { is_expected.to contain_file('/etc/ssmtp/revaliases') }
         it { is_expected.to contain_package('ssmtp') }
+
+        it 'should generate valid content for ssmtp.conf' do
+            content = catalogue.resource('file', '/etc/ssmtp/ssmtp.conf').send(:parameters)[:content]
+            expect(content).to match('ssmtp')
+            expect(content).to match('test@example.local')
+            expect(content).to match('mh.example.local')
+            expect(content).to match('YES')
+        end
       when 'RedHat'
         it { is_expected.to contain_class('ssmtp::package') }
         it { is_expected.to contain_class('ssmtp::config') }
@@ -25,9 +41,19 @@ describe "ssmtp" do
         it { is_expected.to contain_file('/etc/ssmtp/ssmtp.conf') }
         it { is_expected.to contain_file('/etc/ssmtp/revaliases') }
         it { is_expected.to contain_package('ssmtp') }
+
+        it 'should generate valid content for ssmtp.conf' do
+            content = catalogue.resource('file', '/etc/ssmtp/ssmtp.conf').send(:parameters)[:content]
+            expect(content).to match('ssmtp')
+            expect(content).to match('test@example.local')
+            expect(content).to match('mh.example.local')
+            expect(content).to match('YES')
+        end
       else
         it { is_expected.to contain_warning('The current operating system is not supported!') }
       end
     end
   end
 end
+
+at_exit { RSpec::Puppet::Coverage.report! }
