cf2bc0c36dbe098e43798024ca9fc3564e93e345
thbe <project@bendler-net.de>
2017-06-27 12:39:39 +0200

    Fixed test manifest

--- tests/init.pp
+++ tests/init.pp
@@ -9,7 +9,4 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-class { '::ssmtp':
-  mailHub   => 'mail.example.local',
-  rootEmail => 'john.doe@example.local',
-}
+class { '::ssmtp': }
