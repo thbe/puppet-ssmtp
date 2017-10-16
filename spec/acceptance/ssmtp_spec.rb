f8a0b83a8e1ff1462876210633142b6e4ee25510
thbe <project@bendler-net.de>
2017-10-16 16:42:39 +0200

    Upgrade to version 0.5.10, introducing new CI pipeline

---
+++ spec/acceptance/ssmtp_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper_acceptance'
+
+describe 'ssmtp' do
+  let(:manifest) {
+    <<-EOS
+class { 'ssmtp': 
+  default_mta => 'ssmtp',
+  root_email  => 'test@example.local',
+  mail_hub    => 'mh.example.local',
+}
+EOS
+  }
+
+  it 'should apply without errors' do
+    apply_manifest(manifest, :catch_failures => true)
+  end
+
+  it 'should apply a second time without changes' do
+    @result = apply_manifest(manifest)
+    expect(@result.exit_code).to be_zero
+  end
+
+  describe file('/etc/ssmtp/ssmtp.conf') do
+    it { should be_file }
+    it { should exist }
+  end
+
+  describe file('/etc/ssmtp/revaliases') do
+    it { should be_file }
+    it { should exist }
+  end
+  
+  describe package('ssmtp') do
+    it { should be_installed }
+  end
+end
